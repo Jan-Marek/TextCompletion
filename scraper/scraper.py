4919c0b345614d52234fd1d1feef5f709d191b47
unknown <jan.marek.95@gmail.com>
2018-11-22 17:53:54 +0200

    refactored scraper

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,15 +6,15 @@ import sys
 import math
 
 
-def save_thread(url, base_url, directory=""):
+def save_thread(path, base_url, directory=""):
     # clean up directory arg
     if directory != "" and directory[-1] != "/":
         directory += "/"
     if not os.path.exists(directory):
         raise FileNotFoundError
-    fname = url.split("/")[-2] + ".txt"
+    fname = path.split("/")[-2] + ".txt"
     with open(directory + fname, "w", encoding="utf-8") as ofile:    
-        soup = BeautifulSoup(requests.get(url).text, "lxml")
+        soup = BeautifulSoup(requests.get(base_url+path).text, "lxml")
         while True:
             # iterate over all posts on the page
             posts = soup.find_all("blockquote", class_ = "messageText")
@@ -27,32 +27,30 @@ def save_thread(url, base_url, directory=""):
                 break
             soup = BeautifulSoup(requests.get(base_url + next_url.get("href")).text, "lxml")
 
-# TODO: refactor to not have to get all the thread URLs first, instead download when an URL is found
 # to scrape the entire subforum, pass count=math.inf
-def save_subforum(start_url, base_url, directory="", count=1000, verbose=False):
-    soup = BeautifulSoup(requests.get(start_url).text, "lxml")
-    urls = []
-    if verbose:
-        print("getting thread URLs")
-    while len(urls) < count:
+def save_subforum(start_path, base_url, directory="", count=1000, verbose=False):
+    soup = BeautifulSoup(requests.get(base_url+start_path).text, "lxml")
+    saved_count = 0
+    # go through all links on the page, if they are a thread link, save it
+    while saved_count < count:
         threadlinks = soup.find_all("a", class_="PreviewTooltip")
         for link in threadlinks:
             if link.get("href").startswith("threads/"):
-                urls.append(link.get("href"))
+                if verbose:
+                    print("downloading {}/{}".format(saved_count, count))
+                if directory!="" and not os.path.exists(directory):
+                    os.makedirs(directory)
+                save_thread(link.get("href"), base_url, directory)
+                saved_count += 1
+        # move to the next page
         next_url = soup.find("a", text="Next >")
         if next_url is None:
             break
         soup = BeautifulSoup(requests.get(base_url + next_url.get("href")).text, "lxml")
 
-    for idx, url in enumerate(urls):
-        if verbose:
-            print("downloading {}/{}".format(idx, count))
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        save_thread(base_url + url, base_url, directory)
 
 if __name__ == "__main__":
-    start_url = "https://thecatsite.com/forums/cat-behavior.5/"
+    start_path = "forums/cat-behavior.5/"
     base_url = "https://thecatsite.com/"
-    save_subforum(start_url, base_url, "threads", count=math.inf, verbose=True)
+    save_subforum(start_path, base_url, directory="threads", verbose=True)
 
